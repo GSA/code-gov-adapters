69d3e0e2cf625ef2dda2fccef42ab514c565dcc3
Sean Starnes <sean.starne@gsa.gov>
2019-12-02 12:30:48 -0600

    Added test data for util.

--- tests/elasticsearch/utils.test.js
+++ tests/elasticsearch/utils.test.js
@@ -31,8 +31,461 @@ const {
 
 describe('getFlattenedMappingPropertiesByType', () => {
   it('should ', () => {
-    const result = getFlattenedMappingPropertiesByType();
-    expect(result).to.equal(false);
+    const testData = {
+      "properties" : {
+        "additionalInformation" : {
+          "type" : "object",
+          "dynamic" : "true"
+        },
+        "agency" : {
+          "properties" : {
+            "acronym" : {
+              "type" : "keyword",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              }
+            },
+            "codeUrl" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            },
+            "complianceDashboard" : {
+              "type" : "boolean"
+            },
+            "fallback_file" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "ignore_above" : 256
+                }
+              }
+            },
+            "name" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "requirements" : {
+              "properties" : {
+                "agencyWidePolicy" : {
+                  "type" : "float"
+                },
+                "inventoryRequirement" : {
+                  "type" : "float"
+                },
+                "openSourceRequirement" : {
+                  "type" : "float"
+                },
+                "overallCompliance" : {
+                  "type" : "float"
+                },
+                "schemaFormat" : {
+                  "type" : "float"
+                }
+              }
+            },
+            "website" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          }
+        },
+        "contact" : {
+          "properties" : {
+            "URL" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "ignore_above" : 256
+                }
+              }
+            },
+            "email" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "name" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "phone" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "twitter" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            }
+          }
+        },
+        "date" : {
+          "properties" : {
+            "created" : {
+              "type" : "date",
+              "ignore_malformed" : true
+            },
+            "lastModified" : {
+              "type" : "date",
+              "ignore_malformed" : true
+            },
+            "metadataLastUpdated" : {
+              "type" : "date",
+              "ignore_malformed" : true
+            }
+          }
+        },
+        "description" : {
+          "type" : "text",
+          "analyzer" : "englishfulltext"
+        },
+        "disclaimerText" : {
+          "type" : "text",
+          "analyzer" : "englishfulltext"
+        },
+        "disclaimerURL" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "downloadURL" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "events" : {
+          "type" : "text",
+          "fields" : {
+            "keyword" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          },
+          "analyzer" : "englishfulltext"
+        },
+        "homepageURL" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "laborHours" : {
+          "type" : "integer"
+        },
+        "languages" : {
+          "type" : "text",
+          "fields" : {
+            "keyword" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          },
+          "analyzer" : "englishfulltext"
+        },
+        "measurementType" : {
+          "properties" : {
+            "ifOther" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "method" : {
+              "type" : "keyword"
+            }
+          }
+        },
+        "name" : {
+          "type" : "text",
+          "fields" : {
+            "keyword" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          },
+          "analyzer" : "englishfulltext"
+        },
+        "organization" : {
+          "type" : "text",
+          "fields" : {
+            "keyword" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          },
+          "analyzer" : "englishfulltext"
+        },
+        "partners" : {
+          "type" : "nested",
+          "include_in_root" : true,
+          "properties" : {
+            "email" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            },
+            "name" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            }
+          }
+        },
+        "permissions" : {
+          "properties" : {
+            "exemptionText" : {
+              "type" : "text",
+              "analyzer" : "englishfulltext"
+            },
+            "licenses" : {
+              "type" : "nested",
+              "properties" : {
+                "URL" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                },
+                "name" : {
+                  "type" : "text",
+                  "fields" : {
+                    "keyword" : {
+                      "type" : "keyword",
+                      "normalizer" : "lowercase"
+                    }
+                  },
+                  "analyzer" : "englishfulltext"
+                }
+              }
+            },
+            "usageType" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            }
+          }
+        },
+        "relatedCode" : {
+          "type" : "nested",
+          "properties" : {
+            "URL" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            },
+            "codeName" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "ignore_above" : 256
+                }
+              }
+            },
+            "codeURL" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "ignore_above" : 256
+                }
+              }
+            },
+            "isGovernmentRepo" : {
+              "type" : "boolean"
+            },
+            "name" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            }
+          }
+        },
+        "repoID" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "repositoryURL" : {
+          "type" : "keyword"
+        },
+        "reusedCode" : {
+          "type" : "nested",
+          "properties" : {
+            "URL" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            },
+            "name" : {
+              "type" : "text",
+              "fields" : {
+                "keyword" : {
+                  "type" : "keyword",
+                  "normalizer" : "lowercase"
+                }
+              },
+              "analyzer" : "englishfulltext"
+            }
+          }
+        },
+        "score" : {
+          "type" : "integer"
+        },
+        "status" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "tags" : {
+          "type" : "text",
+          "fields" : {
+            "keyword" : {
+              "type" : "keyword",
+              "normalizer" : "lowercase"
+            }
+          },
+          "analyzer" : "englishfulltext"
+        },
+        "targetOperatingSystems" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "vcs" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        },
+        "version" : {
+          "type" : "keyword",
+          "normalizer" : "lowercase"
+        }
+      }
+    };
+
+    const expectedData = {
+      "object": [
+        'additionalInformation'
+      ],
+      "keyword": [
+        'agency.acronym',
+        'agency.codeUrl',
+        'agency.website',
+        'disclaimerURL',
+        'downloadURL',
+        'homepageURL',
+        'measurementType.method',
+        'permissions.licenses.URL',
+        'relatedCode.URL',
+        'repoID',
+        'repositoryURL',
+        'reusedCode.URL',
+        'status',
+        'targetOperatingSystems',
+        'vcs',
+        'version'
+      ],
+      "boolean": [
+        'agency.complianceDashboard',
+        'relatedCode.isGovernmentRepo'
+      ],
+      "text": [
+        'agency.fallback_file',
+        'agency.name',
+        'contact.URL',
+        'contact.email',
+        'contact.name',
+        'contact.phone',
+        'contact.twitter',
+        'description',
+        'disclaimerText',
+        'events',
+        'languages',
+        'measurementType.ifOther',
+        'name',
+        'organization',
+        'partners.email',
+        'partners.name',
+        'permissions.exemptionText',
+        'permissions.licenses.name',
+        'permissions.usageType',
+        'relatedCode.codeName',
+        'relatedCode.codeURL',
+        'relatedCode.name',
+        'reusedCode.name',
+        'tags'
+      ],
+      "float": [
+        'agency.requirements.agencyWidePolicy',
+        'agency.requirements.inventoryRequirement',
+        'agency.requirements.openSourceRequirement',
+        'agency.requirements.overallCompliance',
+        'agency.requirements.schemaFormat'
+      ],
+      "date": [
+        'date.created',
+        'date.lastModified',
+        'date.metadataLastUpdated'
+      ],
+      "integer": [
+        'laborHours',
+        'score'
+      ],
+      "nested": [
+        'partners',
+        'permissions.licenses',
+        'relatedCode',
+        'reusedCode'
+      ]
+    };
+
+    const result = getFlattenedMappingPropertiesByType(testData);
+
+    expect(result).to.deep.equal(expectedData);
   });
 });
 describe('omitPrivateKeys', () => {
